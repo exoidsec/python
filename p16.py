5637eb5ccf66b4d65f17a0d5f13d89e1301264e1
exoidsec <exoidsec@gmail.com>
2026-03-27 12:12:52 +0530

    Update p16.py

--- p16.py
+++ p16.py
@@ -1,1 +1,16 @@
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset('tips')
+
+plt.figure(figsize=(10, 6))
+
+sns.barplot(data=df, x='day', y='tip', palette='husl')
+
+plt.title('Average Tip Amount by Day of the Week')
+plt.xlabel('Day of the Week')
+plt.ylabel('Average Tip ($)')
+
+plt.show()
