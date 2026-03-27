7dbb2a740c10e8774d1d0eff08a95897c51f1e30
exoidsec <exoidsec@gmail.com>
2026-03-27 11:51:08 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,13 +1,14 @@
 import numpy as np
+np.random.seed(28)
+data = np.random.randint(1,1001,size=100)
+data_500_800 = data[(data > 500)&(data <800)]
+mean_data_500_800 = np.mean(data_500_800)
 
-data = np.random.randint(1, 1001, 100)
-
-print("og array:\n", data)
-
-filtered_data = [data > 500]
-
-print("\nFiltered Values (greater than 500):\n", filtered_data)
-
-mean_value = np.mean(filtered_data)
-
-print("\nMean of filtered values:", mean_value)
+print("Original Array: \n",data)
+print("")
+print("Filtered Values (500 < x < 800): ",data_500_800)
+print("")
+print(f"Mean of Filtered data: {mean_data_500_800:.4f}")
+print("")
+d2 = data[(data < 200) | (data > 800)]
+print("D2 = ",d2)
