4026acdd8641ef440305c2df9fdb9ecd5c15c60a
exoidsec <exoidsec@gmail.com>
2026-03-27 12:10:11 +0530

    Update p15.py

--- p15.py
+++ p15.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+df = sns.load_dataset('tips')
+
+corr_matrix = df[['total_bill', 'tip', 'size']].corr()
+
+plt.figure(figsize=(8, 6))
+
+sns.heatmap(
+    corr_matrix,
+    annot=True,
+    fmt='.2f',
+    cmap='coolwarm',
+    center=0,
+    square=True,
+    linewidths=0.5
+)
+
+plt.title("Correlation Heatmap of Tips Dataset")
+
+plt.show()
